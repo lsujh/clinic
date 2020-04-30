c297c478c90fa6b0bafd58d72f0755aa82857053
lsujh <lsujh@i.ua>
2020-04-30 09:13:59 +0300

    Update serializers.py

--- user_account/serializers.py
+++ user_account/serializers.py
@@ -5,7 +5,7 @@ from .models import Pets
 class PetsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pets
-        fields = ('user_id', 'pet_name', 'date_of_birth', 'pet_sex', 'pet_type_id', 'breed_id', 'weight')
+        fields = ('user_id', 'pet_name', 'date_of_birth', 'pet_sex', 'pet_type_id', 'breed_id', 'weight', 'image')
 
 
 
