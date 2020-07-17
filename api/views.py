89f3b9ae088bc70db696a3ead6b7c0a79d1dd7dd
igor@intime.digital <igor@intime.digital>
2020-07-17 10:20:02 +0300

    Default Changelist
    Signed-off-by: Lsujh <lsujh@i.ua>
    Signed-off-by: igor@intime.digital <igor@intime.digital>

--- api/views.py
+++ api/views.py
@@ -1,15 +1,9 @@
-from django.contrib.auth.decorators import login_required
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework import status, generics, response
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.views import APIView
-from django.shortcuts import get_object_or_404
-from .serializers import PetsSerializer
-from .models import Pets
 from rest_framework import viewsets
 from rest_framework import permissions
+
 from .permissions import IsOwnerOrReadOnly
+from .serializers import PetsSerializer
+from .models import Pets
 
 permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
